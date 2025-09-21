bfdc237eed07c54ecca35a8931bb8e233aa9e2b5
luchosale96-tech <luchosale96@gmail.com>
2025-09-21 00:16:01 -0300

    Add files via upload

---
+++ Proyecto de programacion 2/MenuRepuestos.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Proyecto_de_programacion_2
+{
+    partial class MenuRepuestos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MenuRepuestos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 624);
+            this.Name = "MenuRepuestos";
+            this.Text = "MenuRepuestos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
